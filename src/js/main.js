833a122a8445b8b1b35a0feef772998db9be45ba
Nam Yooseong <nys8765@naver.com>
2026-05-12 11:27:06 +0900

    babel-polyfill 설치

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import "@babel/polyfill";
 import { pi, power, Foo } from "./lib";
 
 console.log(pi);
@@ -6,3 +7,13 @@ console.log(power(pi, pi));
 const f = new Foo();
 console.log(f.foo());
 console.log(f.bar());
+
+console.log(
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve(1), 100);
+  }),
+);
+
+console.log(Object.assign({}, { x: 1 }, { y: 2 }));
+
+console.log(Array.from([1, 2, 3], (v) => v + v));
